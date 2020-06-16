97b55784fc2eb56f23addd76d2e38c5743413125
samy280497 <sanyamlala@gmail.com>
2020-06-17 02:34:49 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,44 @@
+// Import express
+let express = require('express');
+// Import Body parser
+let bodyParser = require('body-parser');
+// Import Mongoose
+let mongoose = require('mongoose');
+// Initialize the app
+let app = express();
+
+// Import routes
+let apiRoutes = require("./api-routes");
+// Configure bodyparser to handle post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+// Connect to Mongoose and set connection variable
+mongoose.connect('mongodb://heroku_2t2gdhpl:admin99@ds021299.mlab.com:21299/heroku_2t2gdhpl', { useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost/resthub', { useNewUrlParser: true});
+
+// Heroku Mongoose connection
+// mongoose.connect('mongodb://heroku_2t2gdhpl:admin99@ds349857.mlab.com:49857/heroku_2t2gdhpl', { useNewUrlParser: true });
+
+var db = mongoose.connection;
+
+// Added check for DB connection
+
+if(!db)
+    console.log("Error connecting db")
+else
+    console.log("Db connected successfully")
+
+// Setup server port
+var port = process.env.PORT || 8080;
+
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+// Use Api routes in the App
+app.use('/api', apiRoutes);
+// Launch app to listen to specified port
+app.listen(port, function () {
+    console.log("Running RestHub on port " + port);
+});
